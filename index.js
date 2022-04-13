5ad04213d3cc7d2b6a6779ef09535fab22d55f2e
Your Name <jimmipangalinan@gmail.com>
2022-04-13 18:59:51 +0700

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 // running port
-const port = 5000 ;
+const port = process.env.PORT || 5000 ;
 
 app.use(express.json());
 app.use(cors());
---
+++ seeders/20220413115810-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          fullName: 'admin Handal',
+          email: 'admin@mail.com',
+          password:
+            '$2b$10$7ovHDrtaMe.FmutXxEhnWOo7rDOdTloUMgqms5RXYmL5/4dfM.OTm', //123456
+        },
+      ],
+      {}
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
